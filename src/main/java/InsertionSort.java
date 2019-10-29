47c92d3e7d54dbb1b70a74c3e76a7ee0e5c23eb4
czarkom <mczarkowski@onet.pl>
2019-10-29 13:39:02 +0100

    Insertion sort completed

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -1,6 +1,22 @@
 public class InsertionSort implements SortingInterface {
     @Override
     public void sort(double[] unsortedVector) {
+        for(int i = 1; i < unsortedVector.length; i++){
+            double x = unsortedVector[i];
+            int j = i - 1;
+            while(j >= 0 && x < unsortedVector[j]){
+                unsortedVector[j + 1] = unsortedVector[j];
+                j--;
+            }
+            unsortedVector[j+1] = x;
+        }
+    }
+
+    public static void main(String args[]){
+        InsertionSort sorter = new InsertionSort();
+        double[] vector = {4,213,1,32,87,1.4,25};
+        sorter.sort(vector);
+        for (int i =0; i < vector.length; i++) System.out.println(vector[i]);
 
     }
 }
--- src/test/java/QuickSortTest.java
+++ src/test/java/QuickSortTest.java
@@ -1,8 +1,10 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class QuickSortTest {
 
-    @org.junit.Test
+    @Test
     public void sort() {
     }
 }
