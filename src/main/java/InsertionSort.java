906bd2c833fe586397286493e01546b7f320dd22
czarkom <mczarkowski@onet.pl>
2019-10-29 19:43:24 +0100

    Selection sort completed

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -14,8 +14,9 @@ public class InsertionSort implements SortingInterface {
 
     public static void main(String args[]){
         InsertionSort sorter = new InsertionSort();
-        double[] vector = {4,213,1,32,87,1.4,25};
-        sorter.sort(vector);
+        SelectionSort sorter2 = new SelectionSort();
+        double[] vector = {10,1,27,2.4,-3,241,87,1.2};
+        sorter2.sort(vector);
         for (int i =0; i < vector.length; i++) System.out.println(vector[i]);
 
     }
--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,6 +1,23 @@
 public class SelectionSort implements SortingInterface {
     @Override
     public void sort(double[] unsortedVector) {
+        if(unsortedVector == null || unsortedVector.length < 1) throw new IllegalArgumentException("Please give me normal array");
+        double temp;
+        for (int i = unsortedVector.length - 1; i >= 1; i--) {
+            int max = findMaxValueIndex(unsortedVector, i);
+            if(max != i){
+                temp = unsortedVector[max];
+                unsortedVector[max] = unsortedVector[i];
+                unsortedVector[i] = temp;
+            }
+        }
+    }
 
+    private int findMaxValueIndex(double[] vector, int lastIndex) {
+        int maxValueIndex = 0;
+        for (int i = 1; i <= lastIndex; i++) {
+            if (vector[i] > vector[maxValueIndex]) maxValueIndex = i;
+        }
+        return maxValueIndex;
     }
 }
