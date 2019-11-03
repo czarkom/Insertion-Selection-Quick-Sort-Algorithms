882eb532625fe4f68a45ea092b5b3b8820b438ef
czarkom <mczarkowski@onet.pl>
2019-11-03 21:09:18 +0100

    test for swap added

---
+++ src/test/java/HelpfulMethodsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelpfulMethodsTest {
+
+    @Test
+    public void swapTest() {
+        double[] vector1 = {1,2};
+        double[] vector2 = {2,1};
+        HelpfulMethods.swap(vector1, 0, 1);
+        assertArrayEquals(vector1, vector2, 0);
+    }
+}
