2adae5e57a0a8c2d86fa85c2b47d12a3d2337a92
czarkom <mczarkowski@onet.pl>
2019-11-03 21:13:44 +0100

    imports fixes

--- src/test/java/HelpfulMethodsTest.java
+++ src/test/java/HelpfulMethodsTest.java
@@ -1,13 +1,13 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class HelpfulMethodsTest {
 
     @Test
     public void swapTest() {
-        double[] vector1 = {1,2};
-        double[] vector2 = {2,1};
+        double[] vector1 = {1, 2};
+        double[] vector2 = {2, 1};
         HelpfulMethods.swap(vector1, 0, 1);
         assertArrayEquals(vector1, vector2, 0);
     }
